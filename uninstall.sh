9d739d2ed290681a06209fe989ef74babc072673
dimas.arif.widianto <dimas.arif.widianto@gmail.com>
2024-06-08 03:41:20 +0000

    🔧 (uninstall): replace uninstall.sh with uninstall script for better path handling
    The new `uninstall` script replaces `uninstall.sh` to improve the handling of paths and environment variables. The new script uses `$HOME` to dynamically determine the user's home directory, ensuring that the uninstallation process is more robust and less prone to errors related to hardcoded paths. Additionally, it provides a clearer message for manual PATH cleanup in `.bashrc`.

--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -d ./.setup-idx ]; then rm -rf ./.setup-idx; fi;
-
-echo "Please remove manually this content from PATH in ${HOME}/.bashrc\n"
-echo -e "$(pwd)/.setup-idx/cmd/setup-idx\n"
